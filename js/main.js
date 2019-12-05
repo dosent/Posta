2c0c4d381f778b986d8d61a992adb87f58b9ae10
Ivan Neshin <ivan.neshin@simbirsoft.com>
2019-12-05 20:21:18 +0400

    https://github.com/dosent/Posta/issues/3

--- js/main.js
+++ js/main.js
@@ -1,32 +1,40 @@
-const authorization = "AccessToken I_53pDoc8j_jtSpHBkYkCFMXBGFrdq6R";
-const userAuthorization = "Basic Kzc5OTE0NjE0MDcxOnVzZXI3Mw==";
-const URL_ARCHIVE = "http://localhost:8089/pochta/1.0/archive";
-const URL_BATCH = "http://localhost:8089/pochta/1.0/batch/{key}/shipment";
-const LEVEL_LOG = true;
+//import ("/js/converter.js");
 
-let shipments = [];
-let activeAjaxConnections = 0;
 
+//import ("/js/utils.js");
 function LOGGER(message) {
     if (LEVEL_LOG) {
         console.log(message);
     }
 }
 
+//import {config} from "/js/configuration.js";
+const AUTHORIZATION = "AccessToken I_53pDoc8j_jtSpHBkYkCFMXBGFrdq6R";
+const USER_AUTHORIZATION = "Basic Kzc5OTE0NjE0MDcxOnVzZXI3Mw==";
+const URL_ARCHIVE = "http://localhost:8089/pochta/1.0/archive";
+const URL_BATCH = "http://localhost:8089/pochta/1.0/batch/{key}/shipment";
+const LEVEL_LOG = true;
+
+
+let shipments = [];
+let liteShipments = [];
+let activeAjaxConnections = 0;
+
+
 function getAjax (url,callback) {
     jQuery.ajax({
         type: 'GET',
         beforeSend: function (jqXHR) {
             /* Authorization header */
-            jqXHR.setRequestHeader("Authorization",authorization);
-            jqXHR.setRequestHeader("X-User-Authorization", userAuthorization);
+            jqXHR.setRequestHeader("Authorization",AUTHORIZATION);
+            jqXHR.setRequestHeader("X-User-Authorization", USER_AUTHORIZATION);
             activeAjaxConnections ++;
         },
         url: url,
         error:function (data) {
             activeAjaxConnections --;
             $("#alert").text(data.statusText);
-            LOGGER(data.statusText);
+            utils.LOGGER(data.statusText);
         },
         success: function (data) {
             activeAjaxConnections --;
@@ -64,19 +72,27 @@ function renderData() {
     let i = 1;
     shipments.forEach(function(item, index, array) {
         let messages = item["messages"];
-        let sender = "ПМТУ г.Ульяновск";
         messages.forEach(function (itemMessage) {
             let receiver = itemMessage["str-index-to"] + ", "+ itemMessage["region-to"]+", " + itemMessage["place-to"]+","+ itemMessage["street-to"]+","
-                + itemMessage["house-to"]+ ", " + itemMessage["recipient-name"];
+                + itemMessage["house-to"];
+            let receiverName = itemMessage["recipient-name"];
 
-            let sum = (itemMessage["total-rate-wo-vat"]+itemMessage["total-vat"])/100 + " руб. (включая НДС: "+itemMessage["total-vat"]/100 +" руб.)";
-            addRow(i++,"",itemMessage["barcode"],sender,receiver,sum);
+            let sum = (itemMessage["total-rate-wo-vat"] + itemMessage["total-vat"]) / 100;
+            let vat = itemMessage["total-vat"]/100;
+            addRow(i++,"",itemMessage["barcode"],receiver,receiverName,sum, vat);
         });
     })
 }
 
-function addRow(key,typeMail, barcode,sender,receiver,sum) {
-    let markup = "<tr class='rowShipments'><td>"+key+"</td><td>"+typeMail+"</td><td>" + barcode + "</td><td>" + sender + "</td><td>" + receiver + "</td><td>" + sum + "</td></tr>";
+function addRow(key,typeMail, barcode,receiver,receiverName,sum,vat) {
+    let markup = "<tr class='rowShipments'><td>"+key+"</td><td>"+typeMail+"</td><td>" + barcode + "</td><td>" + receiver + "</td><td>" + receiverName + "</td><td>" + sum + " ("+ vat +")" + "</td></tr>";
+    let liteShipment = new Object();
+    liteShipments["barcode"] = barcode;
+    liteShipments["receiver"] = receiver;
+    liteShipments["receiverName"] = receiverName;
+    liteShipments["sum"] = sum;
+    liteShipments["vat"] = vat;
+    liteShipments.push(liteShipment);
     $("#tableReport tbody").append(markup);
 }
 
@@ -92,3 +108,7 @@ $(document).ajaxComplete(function( event, request, settings ) {
         renderData();
     }
 });
+
+$(document).onload(function () {
+    $("#dynamicTableReport").bootstrapTable("changeLocale", "zh_TW");
+})

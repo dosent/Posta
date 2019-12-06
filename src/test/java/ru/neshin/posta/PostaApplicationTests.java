ae180be29678ade2cef2f6795466635941a96060
Ivan Neshin <ivan.neshin@simbirsoft.com>
2019-12-06 18:14:02 +0400

    https://github.com/dosent/Posta/issues/6

---
+++ src/test/java/ru/neshin/posta/PostaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neshin.posta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ version01/js/utils.js
@@ -0,0 +1,2 @@
+import ("/js/configuration.js");
+

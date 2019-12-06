73cde9c4035844e6f0d57fd08e2b88b80ca828cb
Ivan Neshin <ivan.neshin@simbirsoft.com>
2019-12-06 18:09:48 +0400

    https://github.com/dosent/Posta/issues/6

--- js/configuration.js
+++
@@ -1,5 +0,0 @@
-export const authorization = "AccessToken I_53pDoc8j_jtSpHBkYkCFMXBGFrdq6R";
-export const userAuthorization = "Basic Kzc5OTE0NjE0MDcxOnVzZXI3Mw==";
-export const URL_ARCHIVE = "http://localhost:8089/pochta/1.0/archive";
-export const URL_BATCH = "http://localhost:8089/pochta/1.0/batch/{key}/shipment";
-export const LEVEL_LOG = true;
